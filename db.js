8a753d29146ab44d21458515a54e87b7380c7983
unknown <IsmaelMatias0>
2025-09-25 14:45:22 -0400

    cambios3

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("test", "postgres", "123", {
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "postgres",
 });
 module.exports = sequelize;
